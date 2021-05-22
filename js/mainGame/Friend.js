8b079997be86afe617a57527ec8f906e919797c4
Ajay Vishnu <ajay@Ajays-MacBook-Pro.local>
2021-05-22 20:24:36 +0530

    first commit

---
+++ js/mainGame/Friend.js
@@ -0,0 +1,51 @@
+function Friend() {
+  var gameUI = GameUI.getInstance();
+
+  var element = new Image();
+  element.src = 'images/friends.png';
+
+  this.type;
+  this.x;
+  this.y;
+  this.velX = 2;
+  this.velY = 0;
+  this.grounded = false;
+  this.sX;
+  this.sY = 0;
+  this.width = 32;
+  this.height = 64;
+
+  var that = this;
+
+  this.princess = function(x, y) {
+    that.type = 12;
+    that.sX = 0;
+    that.sY = -6;
+  };
+
+  this.flower = function(x, y) {
+    that.x = x;
+    that.y = y - that.height;
+    that.type = 31;
+    that.sX = 32;
+  };
+    
+  this.draw = function() {
+    gameUI.draw(element, that.sX, that.sY, that.width, that.height, that.x, that.y, that.width, that.height);
+  };
+
+  this.update = function() {
+    if (that.type == 30) {
+      var gravity = 0.2;
+
+      if (that.grounded) {
+        that.velY = 0;
+      }
+
+      that.velY += gravity;
+
+      that.x += that.velX;
+      that.y += that.velY;
+    }
+  };
+}
